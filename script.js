fefdefe33d8b19b726c04f8aa657684b47959f61
IzantMcoronal <izant2020@gmail.com>
2020-11-11 18:11:11 +0100

    1 file modified

--- script.js
+++ script.js
@@ -4,19 +4,37 @@ const changeBackgroundButton = document.getElementById(
   "changeBackgroundButton"
 );
 
-changeBackgroundButton.addEventListener("click", function() {
-  this.style.backgroundColor = getRandomColor();
-});
+//funcion original antes de cambiarla. Con las flechas simplificamos y lo de abajo seria el resultado. Para que funcione tenemos que sustituir el "this" por la constante.
+//changeBackgroundButton.addEventListener("click", function() {
+//  this.style.backgroundColor = getRandomColor();
+//});
+changeBackgroundButton.addEventListener("click", () => (
+changeBackgroundButton.style.backgroundColor = getRandomColor()));
 
-function getRandomColor() {
+// funcion Original antes de cambiarla. Con las flechas simplificamos y lo de abajo seria el resultado.
+//function getRandomColor() {
+//  const letters = getLetters();
+//  let color = '#';
+//  for (var i = 0; i < 6; i++) {
+//    color += letters[Math.floor(Math.random() * 16)];
+//  }
+//  return color;
+//}
+
+let getRandomColor = () => {
   const letters = getLetters();
   let color = '#';
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
-function getLetters(){
-  return '0123456789ABCDEF';
-}
+
+// funcion Original antes de cambiarla. Con las flechas simplificamos y lo de abajo seria el resultado.
+//function getLetters(){
+//  return '0123456789ABCDEF';
+//}
+let getLetters = ()=>'0123456789ABCDEF';
+
+
